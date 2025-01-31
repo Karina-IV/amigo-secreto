f16dddce8143b3efb1ac74069e5f7c21a9df1574
Karina-IV <ivkarina1979@gmail.com>
2025-01-31 02:52:57 -0300

    função para inserir nomes

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 // Criação de um array que armazenará os nomes dos amigos
 let amigos = [];
+// Desenvolvimento de uma função que permita ao usuário inserir um nome no campo de texto e adicioná-lo à lista de amigos
+function adicionar() {
+    // Criação de captura do valor do campo de entrada para obter o texto inserido pelo usuário 
+        let nomeInput = document.getElementById("campoamigo");
+        let nome = nomeInput.value; 
