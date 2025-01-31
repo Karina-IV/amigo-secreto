45d8ada4f51226b4d627b16ef9c3b343642a98a6
Karina-IV <ivkarina1979@gmail.com>
2025-01-31 02:55:28 -0300

    função inserir nome adicionar na lista

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ let amigos = [];
 function adicionar() {
     // Criação de captura do valor do campo de entrada para obter o texto inserido pelo usuário 
         let nomeInput = document.getElementById("campoamigo");
-        let nome = nomeInput.value; 
+        let nome = nomeInput.value;
+      // Implementação de validação para garantir que o campo não esteja vazio
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }   // Adicionar o nome ao array de amigos 
+    amigos.push(nome);
+    atualizarLista(); 
+ // Limpar o campo de entrada
+    nomeInput.value = "";
+}
